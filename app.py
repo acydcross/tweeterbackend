a9320f870d8dc9ddbf47a474a2b04f31e13f9025
acydcross <acydcross@hotmail.com>
2020-12-14 16:02:04 -0700

    Added login post request

--- app.py
+++ app.py
@@ -7,17 +7,21 @@ from flask_cors import CORS
 app = Flask(__name__)
 CORS(app)
 
-@app.route('/api/user', methods=['GET', 'POST', 'PATCH', 'DELETE'])
+@app.route('/api/login', methods=['POST', 'DELETE'])
 def user():
-    if request.method == 'GET':
+    if request.method == 'POST':
         conn = None
         cursor = None
         user = None
+        rows = None
+        user_email = request.json.get('email')
+        user_password = request.json.get('password')
         try:
             conn = mariadb.connect(host=dbcreds.host, password=dbcreds.password, user=dbcreds.user, port=dbcreds.port, database=dbcreds.database)
             cursor = conn.cursor()
-            cursor.execute("SELECT * FROM user")
+            cursor.execute("SELECT * FROM user WHERE email = ? AND password = ?", [user_email, user_password])
             user = cursor.fetchall()
+            rows = cursor.rowcount
             print (user[0][0])
         except Exception as error:
             print("Something went wrong (THIS IS LAZY): ")
@@ -32,6 +36,10 @@ def user():
                 return Response(json.dumps(user, default=str), mimetype="application/json", status=200)
             else:
                 return Response("Something went wrong!", mimetype="text/html", status=500)
+    elif request.method == 'DELETE':
+        conn = None
+        cursor = None
+        rows = None
 
 @app.route('/api/login', methods=['POST', 'DELETE'])
 def login():
